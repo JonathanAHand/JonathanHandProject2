b2b05b9235989bb29d0b93c453c609ced880085e
Jonathan A Hand <jhand1@my.westga.edu>
2025-11-17 22:15:12 -0500

    Add project structure/all class stubs

---
+++ Model/WordAttempt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonathanHandProject2.Model
+{
+    internal abstract class WordAttempt
+    {
+    }
+}
---
+++ Persistence/JsonHighScorePersistenceManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonathanHandProject2.Persistence
+{
+    internal class JsonHighScorePersistenceManager : HighScorePersistenceManager
+    {
+    }
+}
